84721ce361babc4c290758dbcd2f263b138488c2
ruan-cat <1219043956@qq.com>
2024-03-18 09:13:13 +0800

    🐞 fix: 处理其他域名无法访问的bug；

---
+++ about.md
@@ -0,0 +1,10 @@
+# 关于本站
+
+## 多个访问地址
+
+- [自建域名访问](https://java-pilot-base-doc.ruan-cat.com/)
+- [vercel 域名访问](https://java-pilot-base-doc.vercel.app/)
+
+<!-- -  https://ruanzhongnan.github.io/java-pilot-base-doc/ -->
+
+## 使用的技术栈
--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,12 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/java-pilot-base-doc/",
+  /**
+   * 这里不使用github page要求的配置了 会影响其他域名的访问。
+   * - https://vuejs.press/zh/guide/deployment.html#github-pages
+   */
+  // base: "/java-pilot-base-doc/",
+  base: "/",
 
   lang: "zh-CN",
   title: "文档演示",
